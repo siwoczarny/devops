24901fc29a38678b54442a76272933673e71d025
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-03-08 00:53:39 +0100

    Updates

---
+++ precache-manifest.0e72313e2ca0ed78e1f249340fc9189c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2114645aec4b19c7a6655a027c22a0e",
+    "url": "/devops/index.html"
+  },
+  {
+    "revision": "3f2a8ca6fe28a548f1f9",
+    "url": "/devops/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c81340186edc936566bf",
+    "url": "/devops/static/css/main.25e59b7f.chunk.css"
+  },
+  {
+    "revision": "3f2a8ca6fe28a548f1f9",
+    "url": "/devops/static/js/2.029c9b9b.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/devops/static/js/2.029c9b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81340186edc936566bf",
+    "url": "/devops/static/js/main.0f8efb4a.chunk.js"
+  },
+  {
+    "revision": "4d885fd1db6132715edd",
+    "url": "/devops/static/js/runtime-main.7afcc8ca.js"
+  },
+  {
+    "revision": "0ba1839813baf296b4ba856734d5d00a",
+    "url": "/devops/static/media/001.0ba18398.jpg"
+  },
+  {
+    "revision": "69489a60b27e725b2b3be0763dd6ebc4",
+    "url": "/devops/static/media/002.69489a60.jpg"
+  },
+  {
+    "revision": "70ab50a6c058055997b77ee2d7ac2052",
+    "url": "/devops/static/media/003.70ab50a6.jpg"
+  }
+]);
